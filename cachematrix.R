b289b66e553c949227243c7cc6c6c68d0b105464
Ronald Romero Reyes <rromeror@unal.edu.co>
2014-05-22 23:22:16 +0200

    This is my programming assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function makeCacheMatrix creates a special list of three functions which,
+## 1. get the value of matrix "x".
+## 2. set the value of the inverse of matrix "m"
+## 3. get the value of the inverse of matrix "m"
 
-## Write a short comment describing this function
+## The function receives as input argument a square matrix "x" and returns a list of three functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        getmatrix <- function() x                          ## 1. get the value of matrix "x".
+        setinverse <- function(inverse) inv <<- inverse    ## 2. set the value of the inverse of matrix "m"
+        getinverse <- function() inv                       ## 3. get the value of the inverse of matrix "m"
+        list(getmatrix = getmatrix, setinverse = setinverse, getinverse = getinverse) ## a list of functions is returned
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve performs the following functions,
+## 1. gets as input argument a list "x" containing three functions 
+## 2. checks whether the inverse of matrix "x" was already calculated. If not, the inverse of "x" is calculated, stored
+##    in the cache and returned, otherwise, it is just retrieved from the cache without performing any computation.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        inverse <- x$getinverse()             ##checks whether the inverse of matrix "x" was already caculated.
+        
+        if(!is.null(inverse)) {
+             message("getting cached data")
+             return(inverse)                  ## If already calculated, it is just retrieved from the cache.
+         }
+       
+        data <- x$getmatrix()                 ## If not calculated,
+        inverse <- solve(data, ...)           ## the inverse is calculated,
+        x$setinverse(inverse)                 ## stored in the cache,
+        inverse                               ## and returned by the function
 }
